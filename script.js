a25f8c5389c7167f7ba57f036c2de85f93f99f66
Arina231 <arinahonaa119@gmail.com>
2024-02-21 23:51:36 +0600

    attribute

--- script.js
+++ script.js
@@ -54,6 +54,8 @@ message.style.width = '120%';
 
 document.documentElement.style.setProperty('--color-primary', 'orangered');
 
-
 ///Attributes////
- 
+const logo = document.querySelector('.nav__logo');
+
+console.log(logo.alt);
+

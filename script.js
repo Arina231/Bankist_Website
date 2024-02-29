de3ff9939e21e897c2da333450fa9dc972ff958b
Arina231 <arinahonaa119@gmail.com>
2024-02-29 16:02:41 +0600

    page navigatiion

--- script.js
+++ script.js
@@ -64,22 +64,27 @@ btnScrollTo.addEventListener('click', function (e) {
 });
 
 /////////PAge NAViGATION//////////
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({
+//       behavior: 'smooth',
+//     });
+//   });
+// });
 
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+});
 
 // const header = document.querySelector('.header');
 // ///creating elements
